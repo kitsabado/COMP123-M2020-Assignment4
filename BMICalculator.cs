e07f56d8f0e8f3756e2d8c7e42c2fb503b0bb73e
Kristopher Sabado <ksabado@my.centennialcollege.ca>
2020-08-07 21:58:35 -0400

    Add project files.

---
+++ BMICalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_Assignment4
+{
+    public partial class BMICalculator : Form
+    {
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
